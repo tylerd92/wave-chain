57948336051d10df7ee960fd267b92f982157be8
Tyler <tyler@geekingdoing.com>
2018-06-29 06:29:30 -0400

    Creted GET request to retrieve blocks

--- app/index.js
+++ app/index.js
@@ -6,6 +6,12 @@ const HTTP_PORT = process.env.HTTP_PORT || 3001;
 const app = express();
 const bc = new Blockchain();
 
+app.get('/', (req, res) => {
+    res.send('Welcome to Wave Blockchain');
+});
+
 app.get('/blocks', (req, res) => {
     res.json(bc.chain);
 });
+
+app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
