49ca3c6ca938b9bac96b8b941001ed4ff256906c
Tyler <tyler@geekingdoing.com>
2018-06-29 20:32:47 -0400

    Add mine route

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
+app.use(bodyParser.json());
 const bc = new Blockchain();
 
 app.get('/', (req, res) => {
@@ -12,6 +14,13 @@ app.get('/', (req, res) => {
 
 app.get('/blocks', (req, res) => {
     res.json(bc.chain);
+});
+
+app.post('/mine', (req, res) => {
+    const block = bc.addBlock(req.body.data);
+    console.log(`New block added: ${block.toString()}`);
+
+    res.redirect('/blocks') // redirects to blocks route
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
