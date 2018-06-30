fc0ba63278363cdd593644e8624b92f82f480a60
Tyler <tyler@geekingdoing.com>
2018-06-29 21:13:40 -0400

    Create P2P server

--- app/index.js
+++ app/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
+const P2pServer = require('./p2p-server');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
-app.use(bodyParser.json());
 const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome to Wave Blockchain');
@@ -24,3 +27,4 @@ app.post('/mine', (req, res) => {
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
+p2pServer.listen();
