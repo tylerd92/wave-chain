cabca5c9aabf63251d0d3aa22890d5bfaa928fc7
Tyler <tyler@geekingdoing.com>
2018-07-10 06:53:08 -0400

    Add proof of work and dynamic diffculty

--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -15,4 +15,16 @@ describe('Block', () => {
     it('should set the `lastHash` to match the hash ', () => {
         expect(block.lastHash).toEqual(lastBlock.hash);
     });
+
+    it('should generate a hash that matches the difficulty', () => {
+        expect(block.hash.substring(0, block.difficulty)).toEqual('0'.repeat(block.difficulty));
+    });
+
+    it('should lower the difficulty for slowly mined blocks', () => {
+        expect(Block.adjustDifficulty(block, block.timestamp + 360000)).toEqual(block.difficulty - 1);
+    });
+
+    it('should raise the difficulty for quickly mined blocks', () => {
+        expect(Block.adjustDifficulty(block, block.timestamp)).toEqual(block.difficulty + 1);
+    })
 });
