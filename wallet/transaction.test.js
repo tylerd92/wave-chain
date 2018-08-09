c045a2f76312747960af2c31d8406487d5e98eb9
Tyler <tyler@geekingdoing.com>
2018-08-08 22:29:07 -0400

    Add update transaction

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -21,6 +21,19 @@ describe('Transaction', () => {
             .toEqual(amount);
     });
 
+    it('should input the balance of the wallet', () => {
+        expect(transaction.input.amount).toEqual(wallet.balance);
+    });
+
+    it('should validate a valid transaction', () => {
+        expect(Transaction.verifyTransaction(transaction)).toBe(true);
+    });
+
+    it('should invalidate a corrupt transaction', () => {
+        transaction.outputs[0].amount = 50000;
+        expect(Transaction.verifyTransaction(transaction)).toBe(false);
+    });
+
     describe('transacting with an amount that exceeds the balance', () => {
         beforeEach(() => {
             amount = 50000;
@@ -29,6 +42,26 @@ describe('Transaction', () => {
 
         it('should not create the transaction', () => {
             expect(transaction).toEqual(undefined);
+        });
+    });
+
+    describe('updating a transaction', () => {
+        let nextAmount, nextRecipient;
+
+        beforeEach(() => {
+            nextAmount = 20;
+            nextRecipient = 'n3xt-4ddr355';
+            transaction = transaction.update(wallet, nextRecipient, nextAmount);
+        });
+
+        it(`should subtract the next amount from the sender's output`, () => {
+            expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount)
+                .toEqual(wallet.balance - amount - nextAmount);
+        });
+
+        it('should output an amount for the next recipient', () => {
+            expect(transaction.outputs.find(output => output.address === nextRecipient).amount)
+                .toEqual(nextAmount);
         });
     });
 });
