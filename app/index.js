1fae2f573bc852fb1656995c802386857e4954e6
Tyler <tyler@geekingdoing.com>
2018-08-29 20:15:08 -0400

    Add transaction pool to server

--- app/index.js
+++ app/index.js
@@ -11,7 +11,7 @@ const app = express();
 const bc = new Blockchain();
 const wallet = new Wallet();
 const tp = new TransactionPool();
-const p2pServer = new P2pServer(bc);
+const p2pServer = new P2pServer(bc, tp);
 
 app.use(bodyParser.json());
 
@@ -39,7 +39,12 @@ app.get('/transactions', (req, res) => {
 app.post('/transact', (req, res) => {
     const { recipient, amount } = req.body;
     const transaction = wallet.createTransaction(recipient, amount, tp);
+    p2pServer.broadcastTransaction(transaction);
     res.redirect('/transactions');
+});
+
+app.get('/public-key', (req, res) => {
+    res.json({publicKey: wallet.publicKey});
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
--- app/p2p-server.js
+++ app/p2p-server.js
@@ -1,59 +1,86 @@
-const Websocket = require('ws');
+const Websocket = require("ws");
 
 const P2P_PORT = process.env.P2P_PORT || 5001;
-const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
-
+const peers = process.env.PEERS ? process.env.PEERS.split(",") : [];
+const MESSAGE_TYPES = {
+  chain: "CHAIN",
+  transaction: "TRANSACTION"
+};
+// How to run another instance of the servers
 //$ HTTP_PORT=3002 P2P_PORT=5003 PEERS=ws://localhost:5001,ws://localhost:5002 npm run dev
 
 class P2pServer {
-    constructor(blockchain) {
-        this.blockchain = blockchain;
-        this.sockets = [];
-    }
+  constructor(blockchain, transactionPool) {
+    this.blockchain = blockchain;
+    this.transactionPool = transactionPool;
+    this.sockets = [];
+  }
 
-    listen() {
-        const server = new Websocket.Server({port: P2P_PORT});
-        server.on('connection', socket => this.connectSocket(socket));
+  listen() {
+    const server = new Websocket.Server({ port: P2P_PORT });
+    server.on("connection", socket => this.connectSocket(socket));
 
-        this.connectToPeers();
+    this.connectToPeers();
 
-        console.log(`Listening for peer-to-peer connections on: ${P2P_PORT}`);
-    }
+    console.log(`Listening for peer-to-peer connections on: ${P2P_PORT}`);
+  }
 
-    connectToPeers() {
-        peers.forEach(peer => {
-            const socket = new Websocket(peer); 
+  connectToPeers() {
+    peers.forEach(peer => {
+      const socket = new Websocket(peer);
 
-            socket.on('open', () => this.connectSocket(socket));     
-        });
-    }
+      socket.on("open", () => this.connectSocket(socket));
+    });
+  }
 
-    connectSocket(socket) {
-        this.sockets.push(socket); 
-        console.log('Socket connected');
+  connectSocket(socket) {
+    this.sockets.push(socket);
+    console.log("Socket connected");
 
-        this.messageHandler(socket);
+    this.messageHandler(socket);
 
-        this.sendChain(socket);
-    }
+    this.sendChain(socket);
+  }
 
-    messageHandler(socket) { // sets up message event listener
-        socket.on('message', message => {
-            const data = JSON.parse(message); 
+  messageHandler(socket) {
+    socket.on("message", message => {
+      const data = JSON.parse(message);
+      switch (data.type) {
+        case MESSAGE_TYPES.chain:
+          this.blockchain.replaceChain(data.chain);
+          break;
+        case MESSAGE_TYPES.transaction:
+          this.transactionPool.updateOrAddTransaction(data.transaction);
+          break;
+      }
+    });
+  }
 
-            this.blockchain.replaceChain(data);
-        });
-    }
+  sendChain(socket) {
+    socket.send(
+      JSON.stringify({
+        type: MESSAGE_TYPES.chain,
+        chain: this.blockchain.chain
+      })
+    );
+  }
 
-    sendChain(socket) {
-        socket.send(JSON.stringify(this.blockchain.chain));
-    }
+  sendTransaction(socket, transaction) {
+    socket.send(
+      JSON.stringify({
+        type: MESSAGE_TYPES.transaction,
+        transaction
+      })
+    );
+  }
 
-    syncChains() {
-        this.sockets.forEach(socket => {
-            this.sendChain(socket);
-        });
-    }
+  syncChains() {
+    this.sockets.forEach(socket => this.sendChain(socket));
+  }
+
+  broadcastTransaction(transaction) {
+    this.sockets.forEach(socket => this.sendTransaction(socket, transaction));
+  }
 }
 
 module.exports = P2pServer;
