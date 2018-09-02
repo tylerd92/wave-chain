4d5d6049555d5f9b0ea3f2016aad1c8af244b09b
Tyler <tyler@geekingdoing.com>
2018-09-02 19:19:17 -0400

    Add miner rewards for validating transactions

--- app/miner.js
+++ app/miner.js
@@ -1,3 +1,6 @@
+const Transaction = require('../wallet/transaction'); 
+const Wallet = require('../wallet');
+
 class Miner {
   constructor(blockchain, transactionPool, wallet, p2pServer) {
     this.blockchain = blockchain;
@@ -8,10 +11,12 @@ class Miner {
 
   mine() {
     const validTransactions = this.transactionPool.validTransactions();
-    // include a reward for the miner
-    // create a block consisting of the valid transactions
-    // synchronize the chains in the peer to peer server
-    // clear the transaction pool
+    validTransactions.push(
+      Transaction.rewardTransaction(this.wallet, Wallet.blockchainWallet)
+    );
+    const block = this.blockchain.addBlock(validTransactions);
+    this.p2pServer.syncChains()
+    this.transactionPool.clear();
     // broadcast to every miner to clear their transaction pools
   }
 }
--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -8,8 +8,7 @@ describe('TransactionPool', () => {
     beforeEach(() => {
         tp = new TransactionPool();
         wallet = new Wallet();
-        transaction = Transaction.newTransaction(wallet, 'r4nd-4dr355', 30);
-        tp.updateOrAddTransaction(transaction);
+        transaction = wallet.createTransaction('r4nd-4dr355', 30, tp);
     });
 
     it('should add a transaction to the pool', () => {
@@ -22,5 +21,35 @@ describe('TransactionPool', () => {
         tp.updateOrAddTransaction(newTransaction);
 
         expect(JSON.stringify(tp.transactions.find(t => t.id === newTransaction.id))).not.toEqual(oldTransaction);
+    });
+
+    it('should clear transactions', () => {
+        tp.clear();
+        expect(tp.transactions).toEqual([]);
+    })
+
+    describe('mixing valid and corrupt transactions', () => {
+        let validTransactions;
+
+        beforeEach(() => {
+            validTransactions = [...tp.transactions];
+            for (let i = 0; i < 6; i++) {
+                wallet = new Wallet();
+                transaction = wallet.createTransaction('r4nd-4ddr355', 30, tp);
+                if(i % 2 === 0) {
+                    transaction.input.amount = 99999;
+                } else {
+                    validTransactions.push(transaction);
+                }
+            }
+        });
+
+        it('should show a difference between valid and corrupt transactions', () => {
+            expect(JSON.stringify(tp.transactions)).not.toEqual(JSON.stringify(validTransactions));
+        });
+
+        it('should grab valid transactions', () => {
+            expect(tp.validTransactions()).toEqual(validTransactions);
+        });
     });
 });
