299ac2cb114e03d7d7709022a2ff95fabc4112b8
Tyler <tyler@geekingdoing.com>
2018-09-06 06:14:09 -0400

    Add calculate wallet balance

--- wallet/index.js
+++ wallet/index.js
@@ -20,7 +20,9 @@ class Wallet {
         return this.keyPair.sign(dataHash);
     }
 
-    createTransaction(recipient, amount, transactionPool) {
+    createTransaction(recipient, amount, blockchain,transactionPool) {
+        this.balance = this.calculateBalance(blockchain);
+
         if (amount > this.balance) {
             console.log(`Amount: ${amount} exceeds current balance: ${this.balance}`);
             return;
